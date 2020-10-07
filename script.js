16bd5e9df22d2f5abf488994a7f2f72b0c57989c
Jack Murphy <signal.space@protonmail.com>
2020-10-07 08:34:41 -0500

    basic ui layout

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let forcast = $.ajax({
-  url: `https://api.openweathermap.org/data/2.5/forecast?q=bujumbura&cnt=5&units=imperial&appid=07163d7898c795d26765947e0a7370f8`,
+  url: `https://api.openweathermap.org/data/2.5/forecast?q=bujumbura&cnt=5&units=imperial&appid=OW_KEY`,
   method: "GET",
 }).then(function (response) {
   console.log(response);
 });
 let fiveDay = $.ajax({
-  url: `https://api.openweathermap.org/data/2.5/weather?q=bujumbura&cnt=5&units=imperial&appid=07163d7898c795d26765947e0a7370f8`,
+  url: `https://api.openweathermap.org/data/2.5/weather?q=bujumbura&cnt=5&units=imperial&appid=OW_KEY`,
   method: "GET",
 }).then(function (response) {
   console.log(response);
